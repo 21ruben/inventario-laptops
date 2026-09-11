0df1fa7b40619e98dc11bc403afbc552f89228a5
21ruben <127776418+21ruben@users.noreply.github.com>
2026-09-11 03:05:50 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from google.genai import types
 app = Flask(__name__, static_folder='templates')
 
 G# Toma la clave de Render si existe, o usa el valor por defecto si pruebas localmente
-GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY", "TU_NUEVA_API_KEY_AQUI")
+GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY", "AQ.Ab8RN6LV2SRiw7SObVS7WPaJeQ8b5543VewvXUNapdWfkZS3iQ")
 client = genai.Client(api_key=GEMINI_API_KEY)
 
 def init_db():
